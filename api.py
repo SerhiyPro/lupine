06bc99143930a0574d003523046f064f52cd2bea
serg <prots.sergiy7@gmail.com>
2021-07-23 15:20:39 +0300

    Add FBV and CBV request handlers

---
+++ api.py
@@ -0,0 +1,52 @@
+import inspect
+
+from parse import parse
+from webob import Request, Response
+
+
+class API:
+    def __init__(self) -> None:
+        self.routes = {}
+    
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+
+        response = self.handle_request(request)
+
+        return response(environ, start_response)
+    
+    def route(self, path):
+        assert path not in self.routes, "Such route already exists."
+
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        return wrapper
+    
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not found."
+    
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            parsed_result = parse(path, request_path)
+            if parsed_result is not None:
+                return handler, parsed_result.named
+        return None, None
+
+    def handle_request(self, request):
+        response = Response()
+
+        handler, kwargs = self.find_handler(request_path=request.path)
+
+        if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+                if handler is None:
+                    raise AttributeError("Method not allowed", request.method)
+
+            handler(request, response, **kwargs)
+        else:
+            self.default_response(response)
+
+        return response
