4c506b3fb92248b910093f911e08f5b898d378c6
serg <prots.sergiy7@gmail.com>
2021-09-07 12:35:50 +0300

    Add tests for the ORM

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
+import os
 import pytest
 
-from lupine import API
+from lupine import API, Database, Table, Column, ForeignKey
 
 
 @pytest.fixture
@@ -11,3 +12,31 @@ def api():
 @pytest.fixture
 def client(api):
     return api.test_session()
+
+
+@pytest.fixture
+def db():
+    DB_PATH = "./test.db"
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
+    db = Database(DB_PATH)
+    return db
+
+
+@pytest.fixture
+def Author():
+    class Author(Table):
+        name = Column(str)
+        age = Column(int)
+
+    return Author
+
+
+@pytest.fixture
+def Book(Author):
+    class Book(Table):
+        title = Column(str)
+        published = Column(bool)
+        author = ForeignKey(Author)
+
+    return Book
---
+++ tests/test_orm.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+
+def test_create_db(db):
+    assert isinstance(db.conn, sqlite3.Connection)
+    assert db.tables == []
+
+
+def test_define_tables(Author, Book):
+    assert Author.name.type == str
+    assert Book.author.table == Author
+
+    assert Author.name.sql_type == "TEXT"
+    assert Author.age.sql_type == "INTEGER"
+
+
+def test_create_tables(db, Author, Book):
+    db.create(Author)
+    db.create(Book)
+
+    assert Author._get_create_sql() == "CREATE TABLE IF NOT EXISTS author (id INTEGER PRIMARY KEY AUTOINCREMENT, age INTEGER, name TEXT);"
+    assert Book._get_create_sql() == "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY AUTOINCREMENT, author_id INTEGER, published INTEGER, title TEXT);"
+
+    for table in ("author", "book"):
+        assert table in db.tables
