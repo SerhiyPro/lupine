4d3bfbcb63e37c4418ce0f329fbe845676e41b32
serg <prots.sergiy7@gmail.com>
2021-09-08 11:23:04 +0300

    Add object fetching

--- lupine/orm.py
+++ lupine/orm.py
@@ -11,6 +11,39 @@ class Database:
         SELECT_TABLES_SQL = "SELECT name FROM sqlite_master WHERE type = 'table';"
         return [x[0] for x in self.conn.execute(SELECT_TABLES_SQL).fetchall()]
     
+    def all(self, table):
+        sql, fields = table._get_select_all_sql()
+
+        result = []
+        for row in self.conn.execute(sql).fetchall():
+            instance = table()
+            for field, value in zip(fields, row):
+                if field.endswith('_id'):
+                    field = field[:-3]
+                    fk = getattr(table, field)
+                    value = self.get(fk.table, id=value)
+                setattr(instance, field, value)
+            result.append(instance)
+
+        return result
+    
+    def get(self, table, id):
+        sql, fields, params = table._get_select_where_sql(id=id)
+
+        row = self.conn.execute(sql, params).fetchone()
+        if row is None:
+            raise Exception(f"{table.__name__} instance with id {id} does not exist")
+
+        instance = table()
+        for field, value in zip(fields, row):
+            if field.endswith('_id'):
+                field = field[:-3]
+                fk = getattr(table, field)
+                value = self.get(fk.table, id=value)
+            setattr(instance, field, value)
+
+        return instance
+    
     def create(self, table):
         self.conn.execute(table._get_create_sql())
     
@@ -36,6 +69,11 @@ class Table:
         if key in _data:
             return _data[key]
         return super().__getattribute__(key)
+    
+    def __setattr__(self, key, value):
+        super().__setattr__(key, value)
+        if key in self._data:
+            self._data[key] = value
 
 
     @classmethod
@@ -54,6 +92,36 @@ class Table:
         fields = ", ".join(fields)
         name = cls.__name__.lower()
         return CREATE_TABLE_SQL.format(name=name, fields=fields)
+    
+    @classmethod
+    def _get_select_all_sql(cls):
+        SELECT_ALL_SQL = 'SELECT {fields} FROM {name};'
+
+        fields = ['id']
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+            if isinstance(field, ForeignKey):
+                fields.append(name + "_id")
+
+        sql = SELECT_ALL_SQL.format(name=cls.__name__.lower(), fields=", ".join(fields))
+
+        return sql, fields
+    
+    @classmethod
+    def _get_select_where_sql(cls, id):
+        SELECT_WHERE_SQL = 'SELECT {fields} FROM {name} WHERE id = ?;'
+        fields = ["id"]
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+            if isinstance(field, ForeignKey):
+                fields.append(name + "_id")
+
+        sql = SELECT_WHERE_SQL.format(name=cls.__name__.lower(), fields=", ".join(fields))
+        params = [id]
+
+        return sql, fields, params
     
     def _get_insert_sql(self):
         INSERT_SQL = "INSERT INTO {name} ({fields}) VALUES ({placeholders});"
--- tests/test_orm.py
+++ tests/test_orm.py
@@ -57,3 +57,77 @@ def test_save_author_instances(db, Author):
     jack = Author(name="Jack Ma", age=39)
     db.save(jack)
     assert jack.id == 4
+
+
+def test_query_all_authors(db, Author):
+    db.create(Author)
+    john = Author(name="John Doe", age=23)
+    vik = Author(name="Vik Star", age=43)
+    db.save(john)
+    db.save(vik)
+
+    authors = db.all(Author)
+
+    assert Author._get_select_all_sql() == (
+        "SELECT id, age, name FROM author;",
+        ["id", "age", "name"]
+    )
+    assert len(authors) == 2
+    assert type(authors[0]) == Author
+    assert {a.age for a in authors} == {23, 43}
+    assert {a.name for a in authors} == {"John Doe", "Vik Star"}
+
+
+def test_get_author(db, Author):
+    db.create(Author)
+    roman = Author(name="John Doe", age=43)
+    db.save(roman)
+
+    john_from_db = db.get(Author, id=1)
+
+    assert Author._get_select_where_sql(id=1) == (
+        "SELECT id, age, name FROM author WHERE id = ?;",
+        ["id", "age", "name"],
+        [1]
+    )
+    assert type(john_from_db) == Author
+    assert john_from_db.age == 43
+    assert john_from_db.name == "John Doe"
+    assert john_from_db.id == 1
+
+
+def test_get_book(db, Author, Book):
+    db.create(Author)
+    db.create(Book)
+    john = Author(name="John Doe", age=43)
+    arash = Author(name="Arash Kun", age=50)
+    book = Book(title="Building an ORM", published=False, author=john)
+    book2 = Book(title="Scoring Goals", published=True, author=arash)
+    db.save(john)
+    db.save(arash)
+    db.save(book)
+    db.save(book2)
+
+    book_from_db = db.get(Book, 2)
+
+    assert book_from_db.title == "Scoring Goals"
+    assert book_from_db.author.name == "Arash Kun"
+    assert book_from_db.author.id == 2
+
+
+def test_query_all_books(db, Author, Book):
+    db.create(Author)
+    db.create(Book)
+    john = Author(name="John Doe", age=43)
+    arash = Author(name="Arash Kun", age=50)
+    book = Book(title="Building an ORM", published=False, author=john)
+    book2 = Book(title="Scoring Goals", published=True, author=arash)
+    db.save(john)
+    db.save(arash)
+    db.save(book)
+    db.save(book2)
+
+    books = db.all(Book)
+
+    assert len(books) == 2
+    assert books[1].author.name == "Arash Kun"
